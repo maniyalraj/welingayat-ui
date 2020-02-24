e1f29429e03df4f19961295323d2a23c1945bf4c
Rajendra Maniyal <maniyalraj@gmail.com>
2020-02-25 00:32:53 +0530

    URL Change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   // serverUrl:"https://lingayat-test.herokuapp.com/",
-  serverUrl:"http://localhost:8080/"
+  // serverUrl:"http://localhost:8080/"
+  serverUrl:"http://lingayat-route-lingayat-opeshift-project.apps.us-east-1.starter.openshift-online.com/"
 
 };
 
